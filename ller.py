ba8e14b8853e8d9803de7c147e9cf507b5bd09ce
Ian Gemp <imgemp@cs.umass.edu>
2016-02-12 16:00:11 -0500

    Updated ller to accept X with more samples than Y for semi-supervised LLER

--- ller.py
+++ ller.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 from scipy.sparse import eye as speye
+from scipy.sparse import csr_matrix
+from scipy.sparse import bmat
 from scipy.sparse.csgraph import laplacian
 from sklearn.manifold.locally_linear import (
     barycenter_kneighbors_graph, null_space, LocallyLinearEmbedding)
@@ -9,18 +11,20 @@ from sklearn.metrics.pairwise import pairwise_distances, rbf_kernel
 from sklearn.neighbors import NearestNeighbors
 
 
-def ller(X, Y, n_neighbors, n_components, mu=0.5, gamma=None, reg=1e-3,
-         eigen_solver='auto', tol=1e-6, max_iter=100, random_state=None):
+def ller(X, Y, n_neighbors, n_components, mu=0.5, gamma=None,
+         reg=1e-3,eigen_solver='auto', tol=1e-6, max_iter=100,
+         random_state=None):
     """
     Locally Linear Embedding for Regression (LLER)
 
     Parameters
     ----------
     X : ndarray, 2-dimensional
-        The data matrix, shape (num_points, num_dims)
+        The data matrix, shape (num_data_points, num_dims)
 
     Y : ndarray, 1 or 2-dimensional
-        The response matrix, shape (num_points, num_responses).
+        The response matrix, shape (num_response_points, num_responses).
+        Y[0:] is assumed to provide responses for X[:num_response_points]
 
     n_neighbors : int
         Number of neighbors for kNN graph construction.
@@ -70,8 +74,8 @@ def ller(X, Y, n_neighbors, n_components, mu=0.5, gamma=None, reg=1e-3,
         raise ValueError("n_neighbors must be less than number of points")
     if n_neighbors <= 0:
         raise ValueError("n_neighbors must be positive")
-    if Nx != Ny:
-        raise ValueError("X and Y must have same number of points")
+    if Nx < Ny:
+        raise ValueError("X should have at least as many points as Y")
 
     M_sparse = (eigen_solver != 'dense')
 
@@ -89,6 +93,11 @@ def ller(X, Y, n_neighbors, n_components, mu=0.5, gamma=None, reg=1e-3,
     L = laplacian(P, normed=False)
     M /= np.abs(M).max()  # optional scaling step
     L /= np.abs(L).max()
+    if Nx > Ny:
+        # zeros = csr_matrix((Nx-Ny,Nx-Ny),dtype=M.dtype)
+        # L = bmat([[L, None], [None, zeros]])
+        ones = csr_matrix(np.ones((Nx-Ny,Nx-Ny)),dtype=M.dtype)
+        L = bmat([[L, None], [None, ones]])
     omega = M + mu * L
     embedding, lle_error = null_space(omega, n_components, k_skip=1,
                                       eigen_solver=eigen_solver, tol=tol,
@@ -123,6 +132,7 @@ class LLER(LocallyLinearEmbedding):
     def fit(self, X, Y):
         self.nbrs_ = NearestNeighbors(self.n_neighbors,
                                       algorithm=self.neighbors_algorithm)
+
         self.nbrs_.fit(X)
         self.embedding_, self.lle_error_, self.ller_error_ = ller(
             self.nbrs_, Y, self.n_neighbors, self.n_components,
